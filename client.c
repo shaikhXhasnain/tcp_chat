2d5e9ea712a0073ac794b052c02765d723787516
shaikhXhasnain <shaikhhasnain0321@gmail.com>
2026-07-03 20:16:45 +0530

    Final version: Completed TCP chat with endless loop and Emergency Stop button

--- client.c
+++ client.c
@@ -5,30 +5,59 @@
 #include <arpa/inet.h>
 
 int main() {
+    // 1. Get a tin can
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) return 1;
+    if (client_socket == -1) {
+        printf("Oh no! We couldn't build the client tin can!\n");
+        return 1;
+    }
 
+    // 2. Write down the Server's address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080); 
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    // 3. Throw the string!
     int connection_status = connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-    if (connection_status == -1) return 1;
+    if (connection_status == -1) {
+        printf("Oh no! The string missed the window (Connection failed)!\n");
+        return 1;
+    }
 
     printf("Woohoo! We threw the string and connected to the treehouse!\n");
 
-    // --- NEW CODE STARTS HERE ---
+    // 4. Get our whiteboard ready
+    char notepad[256];
 
-    // Step 5: Prepare our message
-    char message[] = "Hello Server! This is the Client speaking!";
+    // 5. Start the endless game of catch
+    while (1) {
+        // --- MY TURN TO TALK ---
+        memset(notepad, 0, sizeof(notepad)); // Erase the whiteboard
+        printf("You: ");
+        fgets(notepad, sizeof(notepad), stdin); // Read keyboard
+        send(client_socket, notepad, strlen(notepad), 0); // Yell into the can
 
-    // Step 6: Yell the message into the tin can!
-    send(client_socket, message, sizeof(message), 0);
-    printf("Message sent!\n");
+        // Check if I pushed the emergency stop button
+        if (strncmp(notepad, "quit", 4) == 0) {
+            printf("You chose to leave the chat. Goodbye!\n");
+            break; 
+        }
 
-    // Step 7: Hang up the tin can when we are done
+        // --- MY TURN TO LISTEN ---
+        memset(notepad, 0, sizeof(notepad)); // Erase the whiteboard
+        recv(client_socket, notepad, sizeof(notepad), 0); // Listen to the can
+        printf("Server says: %s", notepad);
+
+        // Check if the Server pushed the emergency stop button
+        if (strncmp(notepad, "quit", 4) == 0) {
+            printf("The Server ended the chat.\n");
+            break; 
+        }
+    }
+
+    // 6. Clean up
     close(client_socket);
-
+    printf("Tin can safely put away.\n");
     return 0;
 }
--- server.c
+++ server.c
@@ -5,41 +5,76 @@
 #include <arpa/inet.h>
 
 int main() {
+    // 1. Get a tin can
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) return 1; 
+    if (server_socket == -1) {
+        printf("Oh no! We couldn't build the tin can!\n");
+        return 1;
+    }
 
+    // 2. Fill out our address form
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
+    // 3. Glue the address to our window
     int bind_status = bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-    if (bind_status == -1) return 1; 
-    
-    int listen_status = listen(server_socket, 5);
-    if (listen_status == -1) return 1;
-    
-    printf("The treehouse is open! Listening for a friend...\n");
+    if (bind_status == -1) {
+        printf("Oh no! The address wouldn't stick (Bind failed)!\n");
+        return 1;
+    }
 
+    // 4. Hang up the OPEN sign
+    int listen_status = listen(server_socket, 5);
+    if (listen_status == -1) {
+        printf("Oh no! Our ear is blocked (Listen failed)!\n");
+        return 1;
+    }
+    
+    printf("The treehouse is open! Listening for a friend to connect...\n");
+
+    // 5. Wait for the string to hit the window
     int client_socket = accept(server_socket, NULL, NULL);
-    if (client_socket == -1) return 1;
+    if (client_socket == -1) {
+        printf("Oh no! We dropped the string (Accept failed)!\n");
+        return 1;
+    }
 
     printf("We caught the string! A friend connected to us!\n");
 
-    // --- NEW CODE STARTS HERE ---
+    // 6. Get our whiteboard ready
+    char notepad[256];
 
-    // Step 11: Get a blank notepad (buffer) big enough to hold 256 letters
-    char server_notepad[256];
+    // 7. Start the endless game of catch
+    while (1) {
+        // --- MY TURN TO LISTEN ---
+        memset(notepad, 0, sizeof(notepad)); // Erase the whiteboard
+        recv(client_socket, notepad, sizeof(notepad), 0); // Listen to the can
+        printf("Client says: %s", notepad);
 
-    // Step 12: Listen to the new tin can (client_socket) and write it on the notepad
-    recv(client_socket, &server_notepad, sizeof(server_notepad), 0);
+        // Check if the Client pushed the emergency stop button
+        if (strncmp(notepad, "quit", 4) == 0) {
+            printf("The Client left the chat.\n");
+            break; 
+        }
 
-    // Step 13: Read the notepad out loud!
-    printf("The Client says: %s\n", server_notepad);
+        // --- MY TURN TO TALK ---
+        memset(notepad, 0, sizeof(notepad)); // Erase the whiteboard
+        printf("You: ");
+        fgets(notepad, sizeof(notepad), stdin); // Read keyboard
+        send(client_socket, notepad, strlen(notepad), 0); // Yell into the can
 
-    // Step 14: Hang up both tin cans
+        // Check if I pushed the emergency stop button
+        if (strncmp(notepad, "quit", 4) == 0) {
+            printf("You chose to end the chat. Goodbye!\n");
+            break; 
+        }
+    }
+
+    // 8. Clean up
     close(client_socket);
     close(server_socket);
-
-    return 0; 
+    printf("Tin cans safely put away.\n");
+    return 0;
 }
